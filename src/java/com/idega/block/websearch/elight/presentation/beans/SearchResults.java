4ca2e6892ede4f6878d85d5d0e3218ec562355f3
Vytautas Civilis <civilis@idega.com>
2007-06-11 16:08:56 +0000

    some new stuff added.

--- src/java/com/idega/block/websearch/elight/presentation/beans/SearchResults.java
+++ src/java/com/idega/block/websearch/elight/presentation/beans/SearchResults.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.faces.context.FacesContext;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import com.idega.block.websearch.business.WebSearchHitIterator;
 import com.idega.block.websearch.business.WebSearchManager;
 import com.idega.block.websearch.data.WebSearchHit;
 import com.idega.core.cache.IWCacheManager2;
+import com.idega.data.StringInputStream;
 import com.idega.idegaweb.IWMainApplication;
 
 /**
@@ -74,22 +76,61 @@ public class SearchResults implements Serializable {
 		List<SearchResult> results = new ArrayList<SearchResult>();
 		hits.resetPosition();
 		
-		while (hits.hasNextInSet()) {
+		DocumentBuilder doc_builder = getDocumentBuilder();
+		
+		if(doc_builder == null) {
+//			TODO: log and set error message to the user
+		}
+		
+		try {
 			
-			WebSearchHit hit = hits.next();
+			while (hits.hasNextInSet()) {
+				
+				WebSearchHit hit = hits.next();
+				
+				SearchResult result = new SearchResult();
+				result.setUrl(hit.getURL());
+				result.setTitle(hit.getTitle());
+				
+				result.setContents(doc_builder.parse(new StringInputStream(
+						new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?><content>")
+						.append(hit.getContents(query))
+						.append("</content>")
+						.toString()
+				)));
+//				TODO: localize
+				String extra_info = hit.getHREF() + " - " + hit.getContentType() + " - " + /*this.iwrb.getLocalizedString("rank", "rank")*/"rank" + ": " + hit.getRank();
+				result.setExtraInfo(extra_info);
+				results.add(result);
+			}
 			
-			SearchResult result = new SearchResult();
-			result.setUrl(hit.getURL());
-			result.setTitle(hit.getTitle());
-			
-			result.setContents(hit.getContents(query));
-//			TODO: localize
-			String extra_info = hit.getHREF() + " - " + hit.getContentType() + " - " + /*this.iwrb.getLocalizedString("rank", "rank")*/"rank" + ": " + hit.getRank();
-			result.setExtraInfo(extra_info);
-			results.add(result);
+		} catch (Exception e) {
+			// TODO: log
+			e.printStackTrace();
 		}
 		
 		return results;
+	}
+	
+	private DocumentBuilder doc_builder;
+	
+	private DocumentBuilder getDocumentBuilder() {
+		
+		if(doc_builder == null) {
+			
+			try {
+				
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				factory.setNamespaceAware(false);
+				factory.setValidating(false);
+				doc_builder = factory.newDocumentBuilder();
+				
+			} catch (Exception e) {
+				// TODO: log
+				e.printStackTrace();
+			}
+		}
+		return doc_builder;
 	}
 	
 	private List<SearchResult> getMessageToTheUser(String message) {
@@ -107,6 +148,7 @@ public class SearchResults implements Serializable {
 			iwcm2 = IWCacheManager2.getInstance(IWMainApplication.getDefaultIWMainApplication());
 		
 //		TODO: is this map thread safe?
+//		if not @see   Map m = Collections.synchronizedMap(new HashMap()); at http://java.sun.com/j2se/1.4.2/docs/api/java/util/Collections.html#synchronizedMap(java.util.Map)
 		return (Map<String, WebSearchHitIterator>)iwcm2.getCache(hit_iterator_cache);
 	}
 }
